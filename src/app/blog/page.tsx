70a1f002784f0a2f656a8cf47974d2b5bece6813
maciejziemichod <maciej.ziemichod@gmail.com>
2023-11-14 17:46:59 +0100

    chore: add upcoming topics for blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,10 @@ export default function Page() {
             <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-400">
                 Coming soon... (I hope)
             </h1>
+            <p className="pt-5">
+                Upcoming: my Next.js thoughts; TypeScript weirdness; custom
+                error messages in Jest.
+            </p>
         </main>
     );
 }
