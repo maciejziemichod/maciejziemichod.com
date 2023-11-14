a9631fd21259e31f6d2a23a7cc1aff8ba34f6b19
maciejziemichod <maciej.ziemichod@gmail.com>
2023-11-14 17:44:32 +0100

    feat: "finish" blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
     return (
         <main className="max-w-2xl mx-auto px-6 py-12 sm:py-24">
-            hello world
+            <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-cyan-400">
+                Coming soon... (I hope)
+            </h1>
         </main>
     );
 }
